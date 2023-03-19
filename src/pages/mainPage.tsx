48ca1b0d47e641c9035fead43e810fba8eca120c
Farkhod Kodirov <farxod94@gmail.com>
2023-03-20 02:02:49 +0500

    fix: card to comporent

--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import SearchBar from '../components/searchBar';
 import data from '../data.json';
+import Card from '../components/card';
 
 interface State {
   searchResults: typeof data; // Указываем тип для searchResults
@@ -34,30 +35,7 @@ class MainPage extends React.Component<object, State> {
         <SearchBar onSearch={this.handleSearch} />
         <div className="cards">
           {this.state.searchResults.map((car, index) => (
-            <div className="card" key={index}>
-              <div className="card-img">
-                <img src={`assets/cars/${car.image}`} width="220" />
-              </div>
-              <div className="product-title">{car.brand}</div>
-              <div className="product-info">
-                <div className="info category">
-                  <span>Модель</span>
-                  <span>{car.model}</span>
-                </div>
-                <div className="info brand">
-                  <span>Год</span>
-                  <span>{car.year}</span>
-                </div>
-                <div className="info discount">
-                  <span>Цвет</span>
-                  <span>{car.color}</span>
-                </div>
-                <div className="info price">
-                  <span>Цена</span>
-                  <span>{car.price}</span>
-                </div>
-              </div>
-            </div>
+            <Card car={car} key={index} />
           ))}
         </div>
       </div>
